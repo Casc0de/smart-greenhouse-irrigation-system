62ad6a9ffca030091d63f77cdc408bb5c98ec931
“CasCCode” <estebancastro@javerianacali.edu.co>
2025-12-10 19:03:17 -0500

    Se borró la carpeta de arduino para facilidad de merge

--- arduino/src/Horario.cpp
+++
@@ -1,9 +0,0 @@
-#include "Horario.h"
-
-Horario::Horario(char dia, uint8_t horaDeRiego, bool activo)
-    : diaSemana(dia),
-      horaARegar(horaDeRiego),
-      fertiRiego(activo),
-      yaDisparadoHoy(false)
-{
-}
